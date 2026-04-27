09d954a29af56f9bf7409d9ad069fbe58bf0748a
Youssef Essam <yousseif22essam@gmail.com>
2026-04-27 09:43:57 +0300

    Created User & Settings Models

--- src/backend/dashboard/models.py
+++ src/backend/dashboard/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Settings(models.Model):
+    class languageChoices(models.TextChoices):
+        ENGLISH = "EN"
+        ARABIC = "AR"
+        ...
+
+    class currencyChoices(models.TextChoices):
+        US_DOLLAR = "$"
+        EGYPTIAN_POUND = "EGP"
+        ...
+
+    class themeChoices(models.TextChoices):
+        LIGHT = "Light Mode"
+        DARK = "Dark Mode"
+
+    language = models.TextField(choices=languageChoices)
+    currency = models.TextField(choices=currencyChoices)
+    theme = models.TextField(choices=themeChoices)
+
+    cycle_duration = models.IntegerField(null=False)
+    budget_threshold = models.FloatField(null=False)
--- src/backend/login/models.py
+++ src/backend/login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    password = models.TextField(max_length=100)
+    name = models.TextField(max_length=100)
+    email = models.TextField(max_length=100)
+    current_cycle_id = models.ForeignKey(
+        'cycle.Cycle',
+        on_delete=models.PROTECT,
+        null=False
+    )
