46f57933fde72b7c2fcdc20db113d91466a2dc2f
Muhammed-Esmail <muhammedesmail101@gmail.com>
2026-05-04 23:22:54 +0300

    add Onboarding

---
+++ src/backend/cycle/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name='history'
+urlpatterns = [
+    path('', views.index, name='welcome'),
+    path('start_cycle/', views.startCycle, name='start_cycle'),
+]
--- src/backend/cycle/views.py
+++ src/backend/cycle/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+import json
+
+from cycle.services.AllowanceManager import AllowanceManager
+from backend.shared_classes.Cycle import Cycle
 
 # Create your views here.
+def index(request):
+    return render(request, 'pages/onboarding/welcome.html')
+
+def startCycle(request):
+    if request.method == 'POST':
+        
+        amount = request.POST.get('amount')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+
+        newCycle = Cycle(-1, start_date, end_date, amount)
+        allowance_manager = AllowanceManager()
+        allowance_manager.initializeCycle(newCycle)
+
+        
+        return redirect('/dashboard/')
+    
+    return HttpResponse('Invalid Method', status=405)
