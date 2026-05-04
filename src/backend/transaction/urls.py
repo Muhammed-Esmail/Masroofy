255318ecca74f88c269ef14eaaaffc83eda2597e
Youssef Essam <yousseif22essam@gmail.com>
2026-05-04 23:35:48 +0300

    Transaction Management Page

---
+++ src/backend/transaction/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name='transaction'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('log_transaction/', views.log_transaction, name='log_transaction'),
+    path('update_transaction/', views.update_transaction, name='update_transaction'),
+    path('delete_transaction/', views.delete_transaction, name='delete_transaction'),
+]
--- src/backend/transaction/views.py
+++ src/backend/transaction/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from transaction.services import TransactionManager
+from history.services import TransactionCRUD
+from datetime import datetime
+from django.http import JsonResponse
 
-# Create your views here.
+transaction_crud = TransactionCRUD()
+transaction_manager = TransactionManager()
+
+def index(request):
+    return render(request, 'pages/transaction/transaction.html')
+
+def log_transaction(request):
+    amount = int(request.POST.get('amount'))
+    cycle_id = int(request.POST.get('cycle_id'))
+    category_id = int(request.POST.get('category_id'))
+    log_date = datetime.strptime(request.POST.get('log_date'), '%Y-%m-%d').date()
+    description = request.POST.get('description')
+    note = request.POST.get('note')    
+
+    transaction_obj = transaction_crud.createDataObject(
+        id=None,
+        amount=amount,
+        cycle_id=cycle_id,
+        category_id=category_id,
+        log_date=log_date,
+        description=description,
+        note=note
+    )
+    
+    transaction_manager.logTransaction(transaction_obj)
+    return JsonResponse({'success': True})
+
+def update_transaction(request):
+    id=int(request.POST.get('id', None))
+    amount = int(request.POST.get('amount'))
+    cycle_id = int(request.POST.get('cycle_id'))
+    category_id = int(request.POST.get('category_id'))
+    log_date = datetime.strptime(request.POST.get('log_date'), '%Y-%m-%d').date()
+    description = request.POST.get('description')
+    note = request.POST.get('note')    
+
+    transaction_obj = transaction_crud.createDataObject(
+        id=id,
+        amount=amount,
+        cycle_id=cycle_id,
+        category_id=category_id,
+        log_date=log_date,
+        description=description,
+        note=note
+    )
+    
+    transaction_manager.updateTransaction(transaction_obj)
+    return JsonResponse({'success': True})
+
+def delete_transaction(request):
+    id=int(request.POST.get('id'))
+    transaction_manager.deleteTransaction(id)
+    return JsonResponse({'success': True})
