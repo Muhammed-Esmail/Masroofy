ff58f8926ec3759e03005523d6ce7c210e0078ec
Omar-Ghareeb <omarghareeb2005@gmail.com>
2026-05-07 16:32:39 +0300

    Add middleware
    token
    
    Middle Ware Complete

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'backend.middleware.MasroofyAuthMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
